58c3c7937a2ff914c1b410973add7cac142b4192
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2007-12-06 10:50:19 +0000

    added ear target, removed s/s user, fixed supervisor password
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@17 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/framework/resources/sql/MySQL/0300_flexive_base_data.sql
+++ src/framework/resources/sql/MySQL/0300_flexive_base_data.sql
@@ -1,10 +1,2 @@
 -- Optional data for a working base 
 
--- Test user "s"
-INSERT INTO FXS_ACCOUNTS VALUES
-(4,0,'s','ade6cce9baa10421c547578a2c3d32cf9a36fc24','dummy@dummy.com',null,
-SYSDATE(),'3000-01-01 00:00:00','',
-1,SYSDATE(),1,SYSDATE(),TRUE,TRUE,1,'s',TRUE,null,null);
-INSERT INTO FXS_USERGROUPMEMBERS VALUES(4,1);
-
-INSERT INTO FXS_ROLEMAPPING VALUES (4,3,1);
