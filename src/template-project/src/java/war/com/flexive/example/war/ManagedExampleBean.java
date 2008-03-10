2d42aedd3fd33a976cdbf15cd6234ffd302f550a
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2008-03-10 16:15:56 +0000

    FX-120: moved plugins to applications/ directory
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@346 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/template-project/src/java/war/com/flexive/example/war/ManagedExampleBean.java
+++ src/template-project/src/java/war/com/flexive/example/war/ManagedExampleBean.java
@@ -4,6 +4,8 @@ import com.flexive.example.shared.interfaces.EJBExample;
 import com.flexive.shared.EJBLookup;
 import com.flexive.shared.exceptions.FxApplicationException;
 import com.flexive.shared.structure.FxType;
+import com.flexive.faces.FxJsfUtils;
+import com.flexive.faces.beans.SystemBean;
 
 import java.util.Map;
 
@@ -18,5 +20,10 @@ public class ManagedExampleBean {
             instanceCounts = EJBLookup.getEngine(EJBExample.class).getInstanceCounts();
         }
         return instanceCounts;
+    }
+
+    public String getBuildInfo() {
+        // managed bean lookup, asserts that the JSF core components are available in the build path  
+        return FxJsfUtils.getManagedBean(SystemBean.class).getBuildInfo();
     }
 }
