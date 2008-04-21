7f2e55591b42ec224706bd04f2f7732607351143
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2008-04-21 15:05:16 +0000

    removed license headers from examples sources
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@469 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/template-project/src/java/ejb/com/flexive/example/ejb/EJBExampleBean.java
+++ src/template-project/src/java/ejb/com/flexive/example/ejb/EJBExampleBean.java
@@ -1,34 +1,3 @@
-/***************************************************************
- *  This file is part of the [fleXive](R) framework.
- *
- *  Copyright (c) 1999-2008
- *  UCS - unique computing solutions gmbh (http://www.ucs.at)
- *  All rights reserved
- *
- *  The [fleXive](R) project is free software; you can redistribute
- *  it and/or modify it under the terms of the GNU Lesser General Public
- *  License version 2.1 or higher as published by the Free Software Foundation.
- *
- *  The GNU Lesser General Public License can be found at
- *  http://www.gnu.org/licenses/lgpl.html.
- *  A copy is found in the textfile LGPL.txt and important notices to the
- *  license from the author are found in LICENSE.txt distributed with
- *  these libraries.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  For further information about UCS - unique computing solutions gmbh,
- *  please see the company website: http://www.ucs.at
- *
- *  For further information about [fleXive](R), please see the
- *  project website: http://www.flexive.org
- *
- *
- *  This copyright notice MUST APPEAR in all copies of the file!
- ***************************************************************/
 package com.flexive.example.ejb;
 
 import com.flexive.example.shared.interfaces.EJBExample;
