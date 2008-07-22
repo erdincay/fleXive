9631715ed80490623f40d7c32c858314306ba240
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2008-07-22 11:41:02 +0000

    FX-286 - added colorpicker widget
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@624 d35d4baa-7912-0410-b0ae-edacad209e15

---
+++ src/ui/jsf-components/src/java/com/flexive/faces/components/ColorPickerHandler.java
@@ -0,0 +1,46 @@
+package com.flexive.faces.components;
+
+import com.sun.facelets.tag.TagHandler;
+import com.sun.facelets.tag.TagConfig;
+import com.sun.facelets.tag.TagAttribute;
+import com.sun.facelets.FaceletContext;
+import com.sun.facelets.FaceletException;
+import com.sun.facelets.el.VariableMapperWrapper;
+
+import javax.faces.component.UIComponent;
+import javax.faces.FacesException;
+import javax.el.ELException;
+import javax.el.VariableMapper;
+import javax.el.ValueExpression;
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Daniel Lichtenberger, UCS
+ * @version $Rev$
+ */
+public class ColorPickerHandler extends TagHandler {
+    private static final String TEMPLATE = "templates/colorPicker.xhtml";
+
+    private final TagAttribute inputId;
+
+    public ColorPickerHandler(TagConfig config) {
+        super(config);
+        this.inputId = getAttribute("inputId");
+    }
+
+    public void apply(FaceletContext ctx, UIComponent parent) throws IOException, FacesException, FaceletException, ELException {
+        VariableMapper orig = ctx.getVariableMapper();
+        try {
+            final VariableMapperWrapper mapper = new VariableMapperWrapper(orig);
+            final String targetId = inputId != null && StringUtils.isNotBlank(inputId.getValue(ctx))
+                    ? inputId.getValue(ctx) : parent.getClientId(ctx.getFacesContext());
+            mapper.setVariable("inputId", ctx.getExpressionFactory().createValueExpression(targetId, String.class));
+            ctx.setVariableMapper(mapper);
+            ctx.includeFacelet(parent, Thread.currentThread().getContextClassLoader().getResource(TEMPLATE));
+        } finally {
+            ctx.setVariableMapper(orig);
+        }
+    }
+}
