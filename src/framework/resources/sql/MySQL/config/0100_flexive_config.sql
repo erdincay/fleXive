3ce8ee9fb53f898d0c241fcb8537d1a301486f11
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2009-04-15 12:55:03 +0000

    FX-525 (change default mapping of first division to ".*")
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@1187 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/framework/resources/sql/MySQL/config/0100_flexive_config.sql
+++ src/framework/resources/sql/MySQL/config/0100_flexive_config.sql
@@ -18,7 +18,5 @@ INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig', 'root_login', 'administrat
 INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig', 'root_password', '976d934d4c84699020cb9941a38ad01690a23e17');
 INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/datasources', '-2', 'jdbc/flexiveTest');
 INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/datasources', '1', 'jdbc/flexiveDivision1');
-INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/datasources', '2', 'jdbc/flexiveDivision2');
-INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/domains', '1', '(\\d+\\.\\d+\\.\\d+\\.\\d+|localhost)');
-INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/domains', '2', 'www\.flexive-develop2\.com|www2\.flexive-develop2\.com');
+INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/domains', '1', '.*');
 INSERT INTO FXS_CONFIGURATION VALUES('/globalconfig/domains', '-2', '(test division)');
