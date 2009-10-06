df0e9bdc7f11097358678c0709f570ab773dd4c7
mplesser <mplesser@d35d4baa-7912-0410-b0ae-edacad209e15>
2009-10-06 12:38:57 +0000

    FX-678 refractored Postgres
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@1679 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/framework/storages/Postgres/resources/tree/0100_isLeaf.sql
+++
@@ -1,7 +0,0 @@
--- Is the given node a leaf node?
-Create OR REPLACE function tree_isLeaf(nodeId int)
-returns int AS $$ -- deterministic reads sql data
-BEGIN
-  return (select count(*)=0 from FXS_TREE where parent=nodeId);
-END;
-$$ LANGUAGE 'plpgsql';
