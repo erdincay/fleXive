0d7841035a5efaa5378a9cbdbd58005eef185fab
cblasnik <cblasnik@d35d4baa-7912-0410-b0ae-edacad209e15>
2009-10-06 12:47:06 +0000

    lock admin GUI: added general extend duration / unlock options (for marked locks in the query list), human-readable tooltips for creation & expiry dates added
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@1682 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/ui/jsf-backend/java/com/flexive/war/beans/admin/main/LockBean.java
+++ src/ui/jsf-backend/java/com/flexive/war/beans/admin/main/LockBean.java
@@ -34,6 +34,7 @@ package com.flexive.war.beans.admin.main;
 import com.flexive.faces.messages.FxFacesMsgErr;
 import com.flexive.faces.model.FxJSFSelectItem;
 import com.flexive.shared.*;
+import com.flexive.shared.content.FxPK;
 import com.flexive.shared.exceptions.FxApplicationException;
 import com.flexive.shared.exceptions.FxLockException;
 import com.flexive.shared.interfaces.ContentEngine;
@@ -42,9 +43,12 @@ import org.apache.commons.lang.StringUtils;
 
 import javax.faces.model.SelectItem;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Lock administration bean
@@ -62,7 +66,10 @@ public class LockBean implements Serializable {
     private int maxEntries = 100;
     private FxLockType lockType;
     private Map<Object, String> dateMap;
+    private Map<Object, String> readableDateFormat;
     private FxLock selectedLock;
+    private ConcurrentMap<FxPK, Boolean> markedLocks = new ConcurrentHashMap<FxPK, Boolean>();
+    private boolean allMarked = false;
 
     /**
      * Mapped function to return the formatted date of a lock entry
@@ -139,6 +146,22 @@ public class LockBean implements Serializable {
         this.lockType = lockType;
     }
 
+    public ConcurrentMap<FxPK, Boolean> getMarkedLocks() {
+        return markedLocks;
+    }
+
+    public void setMarkedLocks(ConcurrentMap<FxPK, Boolean> markedLocks) {
+        this.markedLocks = markedLocks;
+    }
+
+    public boolean isAllMarked() {
+        return allMarked;
+    }
+
+    public void setAllMarked(boolean allMarked) {
+        this.allMarked = allMarked;
+    }
+
     /**
      * Get the list of lock entries based on user selections
      *
@@ -188,38 +211,159 @@ public class LockBean implements Serializable {
         } catch (FxLockException e) {
             new FxFacesMsgErr(e).addToContext();
         }
+        // synchronise markedLocks map
+        synchMarkedMap();
         return null;
     }
 
     public void unlock() {
-        if( selectedLock == null || !selectedLock.isContentLock())
+        if (selectedLock == null || !selectedLock.isContentLock())
             return;
+
+        unlockRef(selectedLock);
+        performQuery();
+    }
+
+    /**
+     * Action method: Extend the duration of a loose lock by 10 minutes, extend a permanent lock by 1 hour
+     */
+    public void extendLock() {
+        if (selectedLock == null || !selectedLock.isContentLock())
+            return;
+
+        extendLockRef(selectedLock);
+        performQuery();
+    }
+
+    /**
+     * Action method: extend all selected locks
+     */
+    public void extendSelected() {
+        for (FxPK pk : markedLocks.keySet()) {
+            if (markedLocks.get(pk)) {
+                final FxLock l = getLockFromPK(pk);
+                if (l != null)
+                    extendLockRef(l);
+            }
+        }
+        performQuery();
+    }
+
+
+    /**
+     * Action method: unlock selected content instances
+     */
+    public void unlockSelected() {
+        for (FxPK pk : markedLocks.keySet()) {
+            if (markedLocks.get(pk)) {
+                final FxLock l = getLockFromPK(pk);
+                if(l != null)
+                    unlockRef(l);
+            }
+        }
+        performQuery();
+    }
+
+    /**
+     * Refactorisation of the unlocking action
+     *
+     * @param lock the lock t.b. unlocked
+     */
+    private void unlockRef(FxLock lock) {
         try {
-            EJBLookup.getContentEngine().unlock(selectedLock.getLockedPK());
-            performQuery();
+            EJBLookup.getContentEngine().unlock(lock.getLockedPK());
         } catch (FxLockException e) {
             new FxFacesMsgErr(e).addToContext();
         }
     }
 
     /**
-     * Extend the duration of a loose lock by 10 minutes, extend a permanent lock by 1 hour
+     * Retrieve a lock for a given PK
+     *
+     * @param inputPK the input PK
+     * @return returns the lock if found, null otherwise
      */
-    public void extendLock() {
-        if(selectedLock == null || !selectedLock.isContentLock())
-            return;
+    private FxLock getLockFromPK(FxPK inputPK) {
+        for (FxLock l : results) {
+            if (l.getLockedPK().equals(inputPK))
+                return l;
+        }
+        return null;
+    }
 
+    /**
+     * Refactorisation of the lock duration extension alg.
+     *
+     * @param lock the FxLock to extend
+     */
+    private void extendLockRef(FxLock lock) {
         final ContentEngine ce = EJBLookup.getContentEngine();
-        final FxLockType t = selectedLock.getLockType();
+        final FxLockType t = lock.getLockType();
 
         try {
             if (t == FxLockType.Loose)
-                ce.extendLock(selectedLock.getLockedPK(), 10 * 60 * 1000);
+                ce.extendLock(lock.getLockedPK(), 10 * 60 * 1000);
             else if (t == FxLockType.Permanent)
-                ce.extendLock(selectedLock.getLockedPK(), 60 * 60 * 1000);
-            performQuery();
+                ce.extendLock(lock.getLockedPK(), 60 * 60 * 1000);
+
         } catch (FxLockException e) {
             new FxFacesMsgErr(e.getCause()).addToContext();
         }
+    }
+
+    /**
+     * Action method: marks / unmarks all locks in the list
+     */
+    public void markAll() {
+        if (allMarked) {
+            for (FxPK pk : markedLocks.keySet()) {
+                markedLocks.replace(pk, true);
+            }
+        } else {
+            for (FxPK pk : markedLocks.keySet()) {
+                markedLocks.replace(pk, false);
+            }
+        }
+    }
+
+    /**
+     * Create a human readable tooltip for the expires / created dates
+     *
+     * @return the readable date format
+     */
+    public Map<Object, String> getReadableDateFormat() {
+        if (readableDateFormat == null) {
+            readableDateFormat = FxSharedUtils.getMappedFunction(new FxSharedUtils.ParameterMapper<Object, String>() {
+                public String get(Object key) {
+                    try {
+                        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(key);
+                    } catch (Exception e) {
+                        return "unknown";
+                    }
+                }
+            });
+        }
+        return readableDateFormat;
+    }
+
+    /**
+     * Synchronizes the list of results with the marked Ids (GUI)
+     */
+    private void synchMarkedMap() {
+        if (results == null) {
+            markedLocks = new ConcurrentHashMap<FxPK, Boolean>(0);
+            return;
+        }
+
+        ConcurrentMap<FxPK, Boolean> tmp = new ConcurrentHashMap<FxPK, Boolean>(results.size());
+        for (FxLock l : results) {
+            final FxPK resultPK = l.getLockedPK();
+            if (markedLocks.containsKey(resultPK))
+                tmp.put(resultPK, markedLocks.get(resultPK));
+            else
+                tmp.put(resultPK, false);
+        }
+        markedLocks = new ConcurrentHashMap<FxPK, Boolean>(tmp.size());
+        markedLocks.putAll(tmp);
     }
 }
