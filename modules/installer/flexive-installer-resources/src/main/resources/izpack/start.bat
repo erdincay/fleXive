c159694736c4ec62ffe8fbcc7f66fc8b804be615
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2010-03-02 08:20:41 +0000

    Installer: also start a HTTPS connector on port 8443 (required e.g. for WebDAV with basic authentication)
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@1969 d35d4baa-7912-0410-b0ae-edacad209e15

--- modules/installer/flexive-installer-resources/src/main/resources/izpack/start.bat
+++ modules/installer/flexive-installer-resources/src/main/resources/izpack/start.bat
@@ -1,2 +1,5 @@
+REM Start a Jetty http instance on port 8080 and a https instance on port 8443.
+
+
 cd jetty
-java -Dopenejb.configuration=openejb.conf.xml -Xmx200m -jar start.jar
+java -Dopenejb.configuration=openejb.conf.xml -Xmx200m -jar start.jar etc/jetty.xml etc/jetty-ssl.xml
--- modules/installer/flexive-installer-resources/src/main/resources/izpack/start.sh
+++ modules/installer/flexive-installer-resources/src/main/resources/izpack/start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
+#
+# Start a Jetty http instance on port 8080 and a https instance on port 8443.
+#
 
 DIR=$(cd $(dirname $0); pwd -P)
 cd $DIR/jetty
-java -Dopenejb.configuration=openejb.conf.xml -Xmx200m -jar start.jar
+java -Dopenejb.configuration=openejb.conf.xml -Xmx200m -jar start.jar etc/jetty.xml etc/jetty-ssl.xml
  
