e9bacb298964998e891677e8c6fe738f82781256
cblasnik <cblasnik@d35d4baa-7912-0410-b0ae-edacad209e15>
2010-03-22 09:36:36 +0000

    Type options: corrected mysql patch & flexive.properties
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@2037 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/framework/storages/MySQL/resources/patch/patch_1995_2034.sql
+++ src/framework/storages/MySQL/resources/patch/patch_1995_2034.sql
@@ -2,4 +2,4 @@
 -- Change: FXS_TYPE_OPT / type options: change col name from "PASSEDON" to "ISINHERITED"
 -- Author: Christopher Blasnik (cblasnik@flexive.com), UCS - unique computing solutions gmbh (http://www.ucs.at)
 
-ALTER TABLE FXS_TYPE_OPT CHANGE COLUMN PASSEDON ISINHERITED;
+ALTER TABLE FXS_TYPE_OPT CHANGE COLUMN PASSEDON ISINHERITED BOOLEAN;
