ef8fcb66de1925f1abcc60b2afaf481c1c49be1c
dlichtenberger <dlichtenberger@d35d4baa-7912-0410-b0ae-edacad209e15>
2014-10-23 08:36:39 +0000

    Fixed MySQL syntax error in update script 2969-2975
    git-svn-id: http://svn.flexive.org/repository/flexive/trunk@3005 d35d4baa-7912-0410-b0ae-edacad209e15

--- src/framework/storages/MySQL/resources/patch/patch_2969_2975.sql
+++ src/framework/storages/MySQL/resources/patch/patch_2969_2975.sql
@@ -1,5 +1,5 @@
 -- Patch from v2969 -> v2975
 -- Change: history tracker: make account nullable
 -- Author: Markus Plesser (markus.plesser@flexive.com), UCS - unique computing solutions gmbh (http://www.ucs.at)
-ALTER TABLE FXS_HISTORY ALTER COLUMN ACCOUNT DROP NOT NULL;
+ALTER TABLE FXS_HISTORY MODIFY ACCOUNT INTEGER UNSIGNED;
 CREATE INDEX FXI_HISTORY ON FXS_HISTORY(APPLICATION,LOGINNAME,TIMESTP,TYPENAME);
